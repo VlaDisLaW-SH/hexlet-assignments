8fec47062db7fa8c5c5b60c58871edb6d6f041b0
@hexlet/cli <support@hexlet.io>
2024-10-13 15:55:12 +0700

    submit java-advanced-ru/multithreading-java

--- java-advanced-ru/multithreading-java/src/main/java/exercise/App.java
+++ java-advanced-ru/multithreading-java/src/main/java/exercise/App.java
@@ -43,3 +43,36 @@ class App {
     }*/
     // END
 }
+
+/* Solution of teacher
+
+public static Map<String, Integer> getMinMax(int[] numbers) {
+
+        MinThread minThread = new MinThread(numbers);
+        MaxThread maxThread = new MaxThread(numbers);
+
+        minThread.start();
+        LOGGER.log(Level.INFO, "Thread " + minThread.getName() + " started");
+        maxThread.start();
+        LOGGER.log(Level.INFO, "Thread " + maxThread.getName() + " started");
+
+        try {
+            minThread.join();
+            LOGGER.log(Level.INFO, "Thread " + minThread.getName() + " finished");
+            maxThread.join();
+            LOGGER.log(Level.INFO, "Thread " + maxThread.getName() + " finished");
+        } catch (InterruptedException e) {
+            System.out.println("Поток был прерван");
+        }
+
+        Map result = Map.of(
+            "min", minThread.getMin(),
+            "max", maxThread.getMax()
+        );
+
+        LOGGER.log(Level.INFO, "Result: " + result.toString());
+
+        return result;
+    }
+
+ */
